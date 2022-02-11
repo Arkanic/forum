4b00ad77d2e054196919f6f1e63753573c05c6cd
Arkanic <silverfishasaurus@gmail.com>
2022-02-11 04:09:11 +0000

    properly working

--- server/server.ts
+++ server/server.ts
@@ -6,12 +6,20 @@ const config = require("../webpack/webpack.dev");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static("static"));
+
 if(process.env.NODE_ENV == "development") {
     const compiler = webpack(config);
     app.use(webpackDevMiddleware(compiler));
 } else {
     app.use(express.static("buildc"));
 }
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(process.env.PORT || "8080", () => {
     console.log("Server Started.");
