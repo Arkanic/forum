45e4111bebbdf4645c86ec0c7207df56f2dea8fc
Arkanic <silverfishasaurus@gmail.com>
2022-04-06 19:59:35 +0000

    more messages, better captcha

--- server/captcha.ts
+++ server/captcha.ts
@@ -10,7 +10,7 @@ function random(min:number, max:number):number {
 }
 
 export function randLetters(len:number):string {
-    let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let result = "";
     for(let i = 0; i < len; i++) {
         result += alpha.charAt(random(0, alpha.length));
@@ -20,23 +20,23 @@ export function randLetters(len:number):string {
 }
 
 export default function genCaptcha():Captcha {
-    const canvas = createCanvas(400, 100);
+    const canvas = createCanvas(300, 100);
     const ctx = canvas.getContext("2d");
 
     let str = randLetters(6);
 
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 400, 100);
+    ctx.fillRect(0, 0, 300, 100);
 
-    let fs = random(20, 40);
+    let fs = random(30, 40);
     ctx.font = `${fs}px monospace`;
 
     ctx.strokeStyle = "#000111";
 
     ctx.beginPath();
-    ctx.moveTo(random(0, 400), random(0, 100));
+    ctx.moveTo(random(0, 300), random(0, 100));
     for(let i = 0; i < 50; i++) {
-        ctx.lineTo(random(0, 400), random(0, 100));
+        ctx.lineTo(random(0, 300), random(0, 100));
     }
     ctx.stroke();
 
@@ -44,7 +44,7 @@ export default function genCaptcha():Captcha {
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
 
-    let x = random(100, 300);
+    let x = random(100, 200);
     let y = random(45, 55);
     
     ctx.save();
